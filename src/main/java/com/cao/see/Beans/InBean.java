90f1f9bd66a69adfc34b3f01bc2202e9682e6bbf
caoyuan <626795711@qq>
2022-12-13 11:46:17 +0800

    play

---
+++ src/main/java/com/cao/see/Beans/InBean.java
@@ -0,0 +1,15 @@
+package com.cao.see.Beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Puck Cao
+ * @date 2022/12/13 11:40 AM
+ */
+@Component
+public class InBean {
+
+    @Autowired
+    private OutBean outBean;
+}
